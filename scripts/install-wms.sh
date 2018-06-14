ff18cc891b570c0cf15512e099caabd060f73e2b
Thomas Butler <tombutler.development@gmail.com>
2018-06-15 08:31:05 +1000

    add install scripts for wms

---
+++ scripts/install-wms.sh
@@ -0,0 +1,31 @@
+# Install pre-reqs
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+# install psql for WMS database script
+apt-get update && apt-get install -y --no-install-recommends \
+    postgresql-client \
+    && rm -rf /var/lib/apt/lists/*
+
+# install ruamel for yaml parsing
+pip3 install \
+    ruamel.yaml \
+    && rm -rf $HOME/.cache/pip
+
+# Init system
+datacube system init --no-init-users 2>&1
+
+# Add Products @TODO: Make this a variable
+datacube product add /dea-config/prod/products/nrt/landsat/products.yaml
+datacube product add /dea-config/prod/products/nrt/sentinel/products.yaml
+
+# Generate WMS specific config
+PGPASSWORD=$DB_PASSWORD psql \
+    -d $DB_DATABASE \
+    -h $DB_HOSTNAME \
+    -p $DB_PORT \
+    -U $DB_USERNAME \
+    -f create_tables.sql 2>&1
+
+# Run index
+./ update_ranges_wrapper.sh
